773e5e00d0f4a45b160654da6c9b163bd8d8ac2a
Levostap <levostapenkov@gmail.com>
2023-10-25 00:56:52 +0300

    Rotated base to public dir. Added test.js

---
+++ Version1/static/test.js
@@ -0,0 +1,10 @@
+
+function LikeTest(id){
+    var el = document.getElementById(id);
+    el.textContent++;
+}
+
+function DislikeTest(id){
+    var el = document.getElementById(id);
+    el.textContent--;
+}
